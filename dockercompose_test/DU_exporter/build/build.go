4a43a6128163a811f881c7402958a0ea67020bea
pohaha <mouseoskarx7@gmail.com>
2021-10-25 19:43:52 +0300

    add initial source files

---
+++ dockercompose_test/DU_exporter/build/build.go
@@ -0,0 +1,10 @@
+package build
+
+var (
+	// BuildVersion is the tagged version of the binary
+	BuildVersion = "<<< filled in by build >>>"
+	// BuildDate is date and time of the binary build
+	BuildDate = "<<< filled in by build >>>"
+	// BuildCommitSha is git sha hash of the commit from which is the binary built
+	BuildCommitSha = "<<< filled in by build >>>"
+)
---
+++ dockercompose_test/DU_exporter/cmd/root.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/dundee/disk_usage_exporter/build"
+	"github.com/dundee/disk_usage_exporter/exporter"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "disk_usage_exporter",
+	Short: "Prometheus exporter for detailed disk usage info",
+	Long: `Prometheus exporter analysing disk usage of the filesystem
+and reporting which directories consume what space.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		printHeader()
+
+		e := exporter.NewExporter(
+			viper.GetInt("dir-level"),
+			viper.GetString("analyzed-path"),
+		)
+		e.SetIgnoreDirPaths(viper.GetStringSlice("ignore-dirs"))
+		e.RunServer(viper.GetString("bind-address"))
+	},
+}
+
+// Execute runs the command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	flags := rootCmd.PersistentFlags()
+	flags.StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.disk_usage_exporter.yaml)")
+	flags.StringP("bind-address", "b", "0.0.0.0:9995", "Address to bind to")
+	flags.StringP("analyzed-path", "p", "/", "Path where to analyze disk usage")
+	flags.IntP("dir-level", "l", 2, "Directory nesting level to show (0 = only selected dir)")
+	flags.StringSliceP("ignore-dirs", "i", []string{"/proc", "/dev", "/sys", "/run", "/var/cache/rsnapshot"}, "Absolute paths to ignore (separated by comma)")
+
+	viper.BindPFlags(flags)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".disk_usage_exporter" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".disk_usage_exporter")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func printHeader() {
+	log.Printf("Disk Usage Prometheus Exporter %s	build date: %s	sha1: %s	Go: %s	GOOS: %s	GOARCH: %s",
+		build.BuildVersion,
+		build.BuildDate,
+		build.BuildCommitSha,
+		runtime.Version(),
+		runtime.GOOS,
+		runtime.GOARCH,
+	)
+}
